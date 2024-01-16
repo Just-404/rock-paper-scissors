c2a3b027e42570910c674ab2cf0f862bfc759565
Eddy <154628904+Just-404@users.noreply.github.com>
2024-01-16 13:35:06 -0400

    Add new functions and a simple score count
    Created two functions. One to repeat the game whenever there is a tie,
    and another to start the game. Also, I added a score count.

--- script.js
+++ script.js
@@ -12,37 +12,73 @@ function getComputerChoice(){
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let score = 0;
+
 function playerRound(playerSelection = "", computerSelection){
 
-
     if(playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
+        playerScore++;
+        score++;
         return 'You win! Rock beats Scissors.';
     }
     else if(playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock'){
+        playerScore++;
+        score++;
         return 'You win! Paper beats Rock.';
     } 
     else if(playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+        playerScore++;
+        score++;
         return 'You win! Scissors beat Paper.';
     }
     else if(computerSelection == 'rock' && playerSelection.toLowerCase() == 'scissors'){
+        computerScore++;
+        score++;
         return 'You lose! Rock beats Scissors.'
     }
     else if(computerSelection == 'paper' && playerSelection.toLowerCase() == 'rock'){
+        computerScore++;
+        score++
         return 'You lose! Paper beats Rock.'
     }
     else if(computerSelection == 'scissors' && playerSelection.toLowerCase() == 'paper'){
+        computerScore++;
+        score++;
         return 'You lose! Scissors beat Paper.'
     }
     else if(computerSelection == playerSelection){
         console.log("Tie. Try again!");
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Rock, Paper, Scissors")
-        return playerRound(playerSelection, computerSelection);
+        return playAgain();
+    }
+    else{
+        console.log("Unexpected entry. Try again");
+        return playAgain();
     }
 }
 
-const computerSelection = getComputerChoice();
+function playAgain(){
+    const computerSelection = getComputerChoice();
+    let userInput = prompt("Rock, Paper, Scissors");
+    return playerRound(userInput, computerSelection);
+}
 
-let userInput = prompt("Rock, Paper, Scissors");
-console.log(playerRound(userInput, computerSelection));
+function game(){
+    const computerSelection = getComputerChoice();
+    let userInput = prompt("Rock, Paper, Scissors");
+    console.log(playerRound(userInput, computerSelection));
 
+        if (score == 5 && playerScore >= 3) {
+            return console.log("You win!");
+        } else if (score == 5 && computerScore >= 3){
+            return console.log("You lose!");
+        }
+    }
+        
+
+game();
+game();
+game();
+game();
+game();
